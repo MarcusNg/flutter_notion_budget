96ea11912fdc6c38ed95956d2272387b27cba5b4
Marcus Ng <MarcusNg@users.noreply.github.com>
2021-06-05 09:28:09 -0400

    fix empty name field

--- lib/item_model.dart
+++ lib/item_model.dart
@@ -13,9 +13,10 @@ class Item {
 
   factory Item.fromMap(Map<String, dynamic> map) {
     final properties = map['properties'] as Map<String, dynamic>;
+    final nameList = (properties['Name']?['title'] ?? []) as List;
     final dateStr = properties['Date']?['date']?['start'];
     return Item(
-      name: properties['Name']?['title']?[0]?['plain_text'] ?? '?',
+      name: nameList.isNotEmpty ? nameList[0]['plain_text'] : '?',
       category: properties['Category']?['select']?['name'] ?? 'Any',
       price: (properties['Price']?['number'] ?? 0).toDouble(),
       date: dateStr != null ? DateTime.parse(dateStr) : DateTime.now(),
